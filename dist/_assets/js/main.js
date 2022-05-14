f46d89f03e3d80f305e728266084aba3dc598dbd
Mayu Fujimoto <mayu.f.work@gmail.com>
2022-05-14 19:03:45 +0900

    no message

---
+++ dist/_assets/js/main.js
@@ -0,0 +1,13 @@
+//ua判定
+document.addEventListener('DOMContentLoaded', function () {
+})
+window.addEventListener('load', function () {
+	AOS.init({
+		//offset: 300,
+		// delay: 100,
+		duration: 1000,
+		once: true,
+	});
+})
+window.addEventListener('scroll', function () {
+});
